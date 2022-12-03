045a05d05f9b988f4fa890dda829bd54b02cba61
jonbartee <joncb2113@gmail.com>
2022-12-02 17:23:25 -0700

    finished first tutorial video

--- Intro.py
+++ Intro.py
@@ -1,4 +1,4 @@
-name = "Jon"
+""" name = "Jon"
 
 print(name)
 
@@ -11,6 +11,8 @@ for i in range(1,5):
 testlist = list(range(5))
 print(testlist)
 
+
+#Defining functions
 def hello():
     print("Hello")
 
@@ -20,4 +22,23 @@ def hello2():
     name = input("Enter name: ")
     print("Hello " + name)
 
-hello2()
+#hello2()
+
+#if statements
+
+numbers = [1,5,2,4,6,10,3]
+def oddoreven():
+    for number in numbers:
+        if number%2 == 0:
+            print(number, "is even.")
+        else:
+            print(number, "is odd.")
+
+oddoreven() """
+
+#creating a table of numbers
+for row in range(5):
+ #   print(row)
+    for col in range(5):
+        print(col, end='')
+    print()
